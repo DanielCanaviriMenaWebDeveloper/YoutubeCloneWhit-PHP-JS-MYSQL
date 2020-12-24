a03426279201d4461fe0f1c000fa8eb6acded240
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2020-12-23 23:42:02 -0400

    version-2

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
         <div id="pageContainer">
 
             <div id="mastHeadContainer">
+                
                 <button class="navShowHide">
                     <img src="assets/images/icons/menu.png" alt="">
                 </button>
@@ -24,6 +25,24 @@
                 <a class="logoContainer" href="index.php">
                     <img src="assets/images/icons/VideoTubeLogo.png" alt="Site Logo" title="Logo">
                 </a>
+
+                <div class="searchBarContainer">
+                    <form action="search.php" method="GET">
+                        <input type="text" class="searchBar" name="term" placeholder="Buscar">
+                        <button class="searchButton">
+                            <img src="assets/images/icons/search.png" alt="Icono Search" title="Buscar Video">
+                        </button>
+                    </form>
+                </div>
+
+                <div class="rightIcons">
+                    <a href="upload.php">
+                        <img class="upload" src="assets/images/icons/upload.png" alt="upload-icon">
+                    </a>
+                    <a href="#">
+                        <img class="upload" src="assets/images/profilePictures/default.png" alt="profile-picture">
+                    </a>
+                </div>
 
             </div>
 
@@ -34,7 +53,7 @@
             <div id="mainSectionContainer">
 
                 <div id="mainContentContainer">
-                    Hola que tal este es el contenido Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi fugiat explicabo voluptates aut perferendis omnis ratione optio amet quibusdam! Est repellendus voluptates pariatur quibusdam sed quod modi corporis molestiae dolore dolorem ipsum assumenda ducimus repudiandae culpa numquam et accusamus cum, nam officiis accusantium animi eum? Possimus minima architecto ipsam porro vitae provident inventore ratione modi nisi alias magnam earum sequi dolores delectus, necessitatibus soluta! Earum debitis nostrum illum tenetur placeat quo at dicta non esse quae culpa natus voluptate minima, quibusdam voluptas autem iusto repellendus quam adipisci quasi laborum temporibus ad corporis et. Velit ad autem quod distinctio ipsa quidem ducimus, commodi natus ut. Alias maxime minus, sapiente quasi repellendus quidem libero nostrum aperiam labore ipsam necessitatibus, soluta facilis voluptatum cumque! Quo quia, quaerat commodi placeat velit aspernatur labore mollitia fugit ea maiores vel iste architecto nobis rem debitis ipsa! Quisquam, est nihil quo quis esse beatae labore amet quam numquam fuga dolor dolore ea expedita dignissimos deleniti sit animi eum laborum molestiae, a fugit. Nostrum facere obcaecati excepturi cum ab voluptates in suscipit sit, iure tenetur odit rem quis atque voluptatem quisquam quo soluta ut. Fugiat expedita ratione voluptatum minus ea debitis inventore qui vel ab similique ut, earum voluptatibus ducimus? Voluptatum, temporibus unde ut minus facilis iure velit neque nostrum perspiciatis non aperiam optio iste libero, architecto asperiores, sapiente dolore repellat. Deleniti beatae, nam atque aliquid tempore sed dolorem! Earum minus incidunt a tempore accusantium voluptate molestiae doloremque tenetur laborum eaque veniam fugiat, minima quis qui asperiores maxime! Repellat ipsa consequatur reiciendis molestiae tempora sapiente, id dolore quae, doloremque illo veritatis dolorum nostrum, fugiat cupiditate sint doloribus quos debitis. Facere, similique qui illo repellat, ipsa, deserunt ipsam reiciendis id consectetur veniam praesentium quidem sequi ullam. Consequuntur laborum, nesciunt, aliquam dolorem eos nihil tempora quia distinctio earum illum inventore repudiandae accusantium quae, nam voluptatum minus et. Dolorem sequi voluptate debitis laboriosam quidem amet facere maxime quod minima, harum fugit iusto officia earum labore, tempora, saepe iste praesentium unde. Accusantium, quisquam corrupti rerum beatae, alias velit illum placeat nobis odio magnam reprehenderit modi qui! Est recusandae, officia deserunt suscipit doloribus earum eaque nostrum. Distinctio, vel minus. Blanditiis voluptatum explicabo temporibus, delectus optio neque a minima atque ab reiciendis quidem suscipit maxime, voluptates velit nihil. Repellat dignissimos expedita ex voluptas ratione eum iure aliquam quod minus explicabo quae sapiente mollitia fugiat autem sunt quo dolore id tempora, perspiciatis magnam laudantium delectus ullam quis eius? Cumque sit, ipsam commodi dolore provident pariatur deleniti quos atque aliquid soluta doloremque, ab libero in! Qui cupiditate ipsa odio voluptates, nihil totam magni culpa temporibus recusandae laboriosam! Vel assumenda, aliquam saepe possimus inventore dicta impedit dolorum nemo in similique veniam? Magnam placeat, dolorum, perferendis, odit praesentium nemo maxime voluptatum veniam sapiente ratione velit mollitia dolorem nobis iste! Animi perspiciatis ipsa modi, aut nemo voluptatum ut iste, tempore nihil fugiat minus iure dignissimos doloribus dolores commodi exercitationem, expedita delectus quibusdam libero quisquam! Corrupti ab quaerat iusto qui quod? Praesentium, perferendis ad. Asperiores ex facere quae assumenda et?
+                    Hola que tal este es el contenido Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi fugiat explicabo voluptates aut perferendis omnis ratione optio amet quibusdam! Est repellendus voluptates pariatur quibusdam sed quod modi corporis molestiae dolore dolorem ipsum assumenda ducimus repudiandae culpa numquam et accusamus cum, nam officiis accusantium animi eum? Possimus minima architecto ipsam porro vitae provident inventore ratione modi nisi alias magnam earum sequi dolores delectus, necessitatibus soluta! Earum debitis nostrum illum tenetur placeat quo at dicta non esse quae culpa natus voluptate minima, quibusdam voluptas autem iusto repellendus quam adipisci quasi laborum temporibus ad corporis et. Velit ad autem quod distinctio ipsa quidem ducimus, commodi natus ut. Alias maxime minus, sapiente quasi repellendus quidem libero nostrum aperiam labore ipsam necessitatibus, soluta facilis voluptatum cumque! Quo quia, quaerat commodi placeat velit aspernatur labore mollitia fugit ea maiores vel iste architecto nobis rem debitis ipsa! Quisquam, est nihil quo quis esse beatae labore amet quam numquam fuga dolor dolore ea expedita dignissimos deleniti sit animi eum laborum molestiae, a fugit. Nostrum facere obcaecati excepturi cum ab voluptates in suscipit sit, iure tenetur 
                 </div>
 
                 
